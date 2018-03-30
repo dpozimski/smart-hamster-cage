7f0057b58a094b79be096bd80ef1ce96ea9eb6b6
ESKYGROUP\d.pozimski <damian.pozimski@esky.com>
2018-03-30 17:26:38 +0200

    uC test

--- Source/SmartHamsterCage/main.cpp
+++ Source/SmartHamsterCage/main.cpp
@@ -5,13 +5,24 @@
  * Author : d.pozimski
  */ 
 
+#define F_CPU 8000000
+
+#define LED_ON(pin) PORTD |= (1 << pin)
+#define LED_OFF(pin) PORTD &= ~(1 << pin)
+#define LED_TOGGLE(pin) PORTD ^= (1 << pin)
+
 #include <avr/io.h>
+#include <util/delay.h>
 
 
 int main(void)
 {
+	DDRD = 255;
+	
     while (1) 
     {
+		_delay_ms(16);
+		LED_TOGGLE(PD6);
     }
 }
 
