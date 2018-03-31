e3323fd60a92bc4990456c46bc35817dae1e3424
ESKYGROUP\d.pozimski <damian.pozimski@esky.com>
2018-03-31 21:43:48 +0200

    Power supply

--- Source/SmartHamsterCage/main.cpp
+++ Source/SmartHamsterCage/main.cpp
@@ -11,17 +11,42 @@
 #define LED_OFF(pin) PORTD &= ~(1 << pin)
 #define LED_TOGGLE(pin) PORTD ^= (1 << pin)
 
+#include <stdbool.h>
+
 #include <avr/io.h>
 #include <util/delay.h>
+#include <stdlib.h>
 
 
 int main(void)
 {
 	DDRD = 255;
 	
-    while (1) 
-    {
-		_delay_ms(16);
+	int idx = 0;
+	bool state = true;
+	srand(53543);
+	while (1)
+	{
+		if(state && idx < 150)
+		{
+			idx++;
+		}
+		else if(!state)
+		{
+			idx--;
+		}
+		
+		for(int i = 0; i < idx; i++)
+		{
+			_delay_ms(1);
+		}
+		
+		if(idx >= 150)
+			state = false;
+			
+		if(idx <= 0)
+			state = true;
+		
 		LED_TOGGLE(PD6);
     }
 }
