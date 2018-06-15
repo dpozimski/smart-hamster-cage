0dd84bb7da527d4bcb320298be2e967a660a53b8
ESKYGROUP\d.pozimski <damian.pozimski@esky.com>
2018-06-15 18:44:35 +0200

    max pwm water pouring to 20

--- Source/SmartHamsterCage/Flow/WaterFeedController.cpp
+++ Source/SmartHamsterCage/Flow/WaterFeedController.cpp
@@ -5,7 +5,7 @@
  *  Author: d.pozimski
  */ 
 
-#define POUR_PWM_VALUE 100
+#define POUR_PWM_VALUE 20
 #define TEMP_EXCEEDED_VALUE 32
 
 #define POUR_CYCLIC_DURATION 175
@@ -30,7 +30,7 @@ void WaterFeedController::setTemperature(uint8_t temperature)
 void WaterFeedController::update()
 {
     uint32_t elapsedSeconds = this->getElapsedSeconds();
-    this->outputTimeRule->update(elapsedSeconds, POUR_DURATION, getDurationOfWaterPouring(elapsedSeconds));
+    this->outputTimeRule->update(elapsedSeconds, POUR_DURATION, getIdleTime(elapsedSeconds));
     
     if(this->outputTimeRule->isOutputShouldBeEnabled() && !this->waterPump->readValue())
     {
@@ -44,7 +44,7 @@ void WaterFeedController::update()
     }
 }
 
-uint8_t WaterFeedController::getDurationOfWaterPouring(uint32_t elapsedSeconds)
+uint8_t WaterFeedController::getIdleTime(uint32_t elapsedSeconds)
 {
     uint8_t cyclicDuration = (this->temperature >= TEMP_EXCEEDED_VALUE) ? 
         POUR_CYCLIC_DURATION_IF_TEMP_EXCEEDED : POUR_CYCLIC_DURATION;
